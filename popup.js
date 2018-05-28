ec8f47b9b04c83a280ed7f3e28539be14def7f91
tejeshreddy <tejeshreddymeka22@gmail.com>
2018-05-28 19:55:09 +0530

    adding getNotes function

--- popup.js
+++ popup.js
@@ -1,41 +1,64 @@
 
-function getSearch(callback){
-	chrome.storage.sync.get("searchOption",callback);
-}
+	function getSearch(callback){
+		chrome.storage.sync.get("searchOption",callback);
+	}
 
-function getColor(callback){
-	chrome.storage.sync.get("colorInversion",callback);
-}
+	function getColor(callback){
+		chrome.storage.sync.get("colorInversion",callback);
+	}
 
+	function getNotes(callback){
+		chrome.storage.sync.get("notes",callback);
+	}
 
-document.addEventListener('DOMContentLoaded', function() {
+	document.addEventListener('DOMContentLoaded', function() {
 
-	var searchBox = document.getElementById("search");
-	var colorBox = document.getElementById("colorInversion");
-	searchBox.addEventListener("click",function(){
-		var checked = document.getElementById("search").checked;
-		chrome.storage.sync.set({ searchOption: checked });
+		var searchBox = document.getElementById("search");
+		searchBox.addEventListener("click",function(){
+			var checked = document.getElementById("search").checked;
+			chrome.storage.sync.set({ searchOption: checked });
+		});
+		
+		var colorBox = document.getElementById("colorInversion");
+		colorBox.addEventListener("click",function(){
+			var checked = document.getElementById("colorInversion").checked;
+			chrome.storage.sync.set({colorInversion:checked});
+			//console.log("color");
+			//console.log(checked);
+		});
+			 
+		
+		var notesBox = document.getElementById("notes");
+		notesBox.addEventListener("click",function(){
+			var checked = document.getElementById("notes").checked;
+			chrome.storage.sync.set({notes:checked});
+			console.log("notes");
+			console.log(cheked);
+		});
+		
+		getSearch(function(searchOption){
+			document.getElementById("search").checked = searchOption['searchOption'];   
+		});
+
+		
+		getColor(function(colorInversion){
+			document.getElementById("colorInversion").checked = colorInversion['colorInversion'];	
+		});
+		
+		getNotes(function(notes){
+			document.getElementById("notes").checked = notes['notes'];
+			//if(document.getElementById("notes").checked === true){
+			//	chrome.tabs.create({'url': "/options.html" } );
+			//}
+		});
 
 	});
-	
-	
-	colorBox.addEventListener("click",function(){
-		var checked = document.getElementById("colorInversion").checked;
-		chrome.storage.sync.set({colorInversion:checked});
-		console.log("color");
-		console.log(checked);
-	});
-		 
-	 
-	getSearch(function(searchOption){
-		document.getElementById("search").checked = searchOption['searchOption'];   
-	});
-
-	
-	getColor(function(colorInversion){
-		document.getElementById("colorInversion").checked = colorInversion['colorInversion'];	
-	});
-	
 
 
-});
+
+
+
+
+
+
+
